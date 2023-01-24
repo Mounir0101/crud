bdec026d719cb41fbfc8d861dd47b136cbbf71c9
mounir <mounirxchitour@gmail.com>
2023-01-24 15:29:54 +0100

    menu

--- src/main/java/com/codingf/Main.java
+++ src/main/java/com/codingf/Main.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
@@ -25,12 +26,58 @@ public class Main {
         Connection connection = DriverManager.getConnection(URL, username, password);
         if (connection == null) {
             System.out.println("Erreur de connexion !!");
+        }else {
+            System.out.println("connexion good !!");
 
+        }
+        Scanner nb = new Scanner(System.in);
+        int table;
+
+        while (true) {
+
+            System.out.println("1: Country");
+            System.out.println("2: City");
+            System.out.println("Quelle table voulez vous choisir ?");
+
+            String input = nb.nextLine();
+
+            try {
+                table = Integer.parseInt(input);
+                break;
+            }
+            catch (Exception e) {
+                System.err.println("nombre incorrect");
+            }
 
         }
 
+        int choice;
+
+        while (true) {
+            System.out.println("1: Créer");
+            System.out.println("2: Lire");
+            System.out.println("3: Mettre à jour");
+            System.out.println("4: Supprimer");
+            System.out.println("que voulez vous faire avec cette table ?");
+
+            String input = nb.nextLine();
+
+            try {
+                choice = Integer.parseInt(input);
+                break;
+            }
+            catch (Exception e) {
+                System.err.println("nombre incorrect");
+            }
+        }
 
 
+        switch (table) {
+
+            case 1:
+
+
+        }
 
     }
 }
